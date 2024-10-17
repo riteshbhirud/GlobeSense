79f7a4d49299840cfde81d64a86397b56fd8fa03
aryand15 <aryandeshpande15@gmail.com>
2024-10-16 22:28:14 -0400

    added mostly functional replay button

--- script.js
+++ script.js
@@ -11,12 +11,35 @@ const gameDisplay = document.getElementById("gameDisplay");
 const gameOverScreen = document.getElementById("gameOverScreen")
 let GameOver = false;
 let marker; 
+let ActualLocationMarker;
 var latlng;
 let streetViewStatus = false;
 let songName;
 let artistName;
+var timer;
 const parentMap = document.getElementById("parentMap");
 //const map = document.getElementById("map");
+let map;
+let panorama;
+
+function timer1(){
+  console.log("TIMER FUNCTION BEING CALLED")
+  var sec = 30;
+  timer = setInterval(function(){
+    let minutes = Math.floor(sec / 60)
+    let seconds = sec - minutes*60
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+      document.getElementById('timer').innerHTML=`${minutes}:${seconds}`;
+      sec--;
+      if (sec < 0) {
+        clearInterval(timer);
+        endGame();
+        
+      }
+  }, 1000);
+}
 
 let encodedCountry;
 let url;
@@ -101,61 +124,6 @@ function wait(ms) {
 async function getRandomCoordinates() {
   let geometry = null;
 
-  // Latitude ranges for major continents (excluding Antarctica)
-  /*const latitudeRanges = [
-    { min: 15, max: 70 },  // North America (USA, Canada, Mexico)
-    { min: -55, max: 15 }, // South America (Argentina to Northern South America)
-    { min: 40, max: 70 },  // Europe (Southern to Northern Europe)
-    //{ min: -35, max: 35 }, // Africa (Southern Africa to Northern Africa)
-    { min: 5, max: 80 },   // Asia (Southeast Asia to Northern Russia)
-    { min: -40, max: -15 }, // Australia region
-    { min: -34.833, max: -22.125 } //SA
-  ];
-
-  // Longitude ranges for major continents (excluding Antarctica)
-  const longitudeRanges = [
-    { min: -170, max: -50 }, // North America (Western Alaska to Eastern US)
-    { min: -80, max: -35 },  // South America (Western South America to Brazil)
-    { min: -25, max: 60 },   // Europe (Western Europe to Eastern Europe)
-    //{ min: -20, max: 55 },   // Africa (West Africa to Eastern Africa)
-    { min: 30, max: 180 },   // Asia (Middle East to Far East Asia)
-    { min: 110, max: 150 },   // Australia to New Zealand
-    { min: 16.449, max: 32.8917 }//SA
-    
-  ];*/
-
-  /*const latitudeRanges = [
-    { min: 24.396308, max: 49.384358 }, // Continental USA
-    /*{ min: 36.578581, max: 71.538800 }, // Canada
-    { min: 14.53125, max: 32.715736 }, // Mexico
-    { min: -55.0, max: 12.0 }, // South America (general coverage)
-    { min: 35.0, max: 71.0 }, // Europe
-    { min: -35.0, max: 37.0 }, // Africa (general coverage)
-    { min: -11.0, max: 45.0 }, // Asia (general coverage)
-    { min: -60.0, max: -10.0 }, // Australia and Oceania (general coverage)
-    { min: 1.0, max: 10.0 }, // Southeast Asia
-    { min: 50.0, max: 60.0 }, // Northern Europe
-  ]
-  
-  const longitudeRanges = [
-    { min: -123.0, max: -68.93457 }, // Continental USA
-    /*{ min: -141.0, max: -52.0 }, // Canada
-    { min: -118.4, max: -86.7 }, // Mexico
-    { min: -81.0, max: -35.0 }, // South America
-    { min: -25.0, max: 40.0 }, // Europe
-    { min: -18.0, max: 51.0 }, // Africa
-    { min: 60.0, max: 180.0 }, // Asia
-    { min: 110.0, max: 180.0 }, // Australia and Oceania
-    { min: 100.0, max: 180.0 }, // Southeast Asia
-    { min: 0.0, max: 25.0 }, // Northern Europe
-  ];*/
-
-
-
-  // Randomly pick a range from the arrays
-  /*const choice = Math.floor(Math.random() * latitudeRanges.length)
-  console.log("ORIGINAL",choice);*/
-
   let city = cities[Math.floor(Math.random() * cities.length)]
   
 
@@ -228,6 +196,7 @@ async function getRandomCoordinates() {
 }
 
 async function fetchCountry() {
+  console.log("CALLING FETCH COUNTRY FUNCTION")
   if (retryCount >= maxRetries) {
     console.error('Max retry limit reached. Stopping further attempts.');
     return null;
@@ -238,50 +207,6 @@ async function fetchCountry() {
   console.log("fetchCountry coordinates", coordinates);
   latitude = coordinates.latitude;
   longitude = coordinates.longitude;
-  //console.log("fetchCountry latitude:", latitude);
-  //console.log("fetchCountry longitude:", longitude);
-  //const apiUrl = `https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&api_key=${geocodeAPIKey}`;
-
-  /*try {
-    const response = await fetch(apiUrl);
-    if (response.status === 429) {
-      console.warn('Too many requests. Retrying after 2 seconds...');
-      await wait(2000); // Retry after 2 seconds
-      return fetchCountry();
-    }
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    const data = await response.json();
-    if (data.hasOwnProperty('error')) {
-      console.warn('Error in response, fetching a new location...');
-      await wait(2000); // Wait 2 seconds before fetching a new location
-      return fetchCountry();
-    } else {
-      const country = data.address.country || 'Country not found';
-      const streetViewApiUrl = `https://maps.googleapis.com/maps/api/streetview/metadata?location=${latitude},${longitude}&key=${googleAPIKey}&radius=100`;
-      const streetViewResponse = await fetch(streetViewApiUrl);
-
-      if (!streetViewResponse.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const streetViewData = await streetViewResponse.json();
-      if (streetViewData.status === "OK") {
-        console.log("GOOGLE STREET VIEW SUCCESSFUL:", latitude, longitude, "are able to be displayed by google street view.")
-        initialize(latitude, longitude);
-        return country;
-      } else if (streetViewData.status === "ZERO_RESULTS") {
-        console.warn('No Street View coverage at this location. Fetching new location...');
-        return fetchCountry();
-      }
-    }
-  } catch (error) {
-    console.error('Fetch error:', error);
-    await wait(2000); // Retry after 2 seconds
-    return fetchCountry();
-  }*/
 
   try {
     const streetViewApiUrl = `https://maps.googleapis.com/maps/api/streetview/metadata?location=${latitude},${longitude}&key=${googleAPIKey}&radius=100`;
@@ -295,7 +220,9 @@ async function fetchCountry() {
     if (streetViewData.status === "OK") {
       streetViewStatus = true;
       console.log("GOOGLE STREET VIEW SUCCESSFUL:", latitude, longitude, "are able to be displayed by google street view.")
+      document.getElementById("timer").style.display = "block";
       initialize(parseFloat(latitude), parseFloat(longitude));
+      timer1();
       return encodedCountry;
     } else if (streetViewData.status === "ZERO_RESULTS") {
       console.warn('No Street View coverage at this location. Fetching new location...');
@@ -312,6 +239,7 @@ async function fetchCountry() {
 
 
 async function startProcess() {
+    console.log("CALLING START PROCESS FUNCTION")
     const country = await fetchCountry();
     if (country) {
       encodedCountry = encodeURIComponent(country);
@@ -381,7 +309,7 @@ async function startProcess() {
   function initialize(lat, lng) {
     const fenway = { lat: lat, lng: lng };
 
-    const panorama = new google.maps.StreetViewPanorama(
+    panorama = new google.maps.StreetViewPanorama(
       document.getElementById("pano"),
       {
         position: fenway,
@@ -439,7 +367,7 @@ async function startProcess() {
   document.addEventListener("DOMContentLoaded", function () {
     // Initialize Leaflet map 
     console.log("reachedreachedreached")
-    var map = L.map('map').setView([20, 0], 2); // World view with center at [20, 0] and zoom level 2
+    map = L.map('map').setView([20, 0], 2); // World view with center at [20, 0] and zoom level 2
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoicmJoaXJ1ZCIsImEiOiJjbTF1NzZ3NDMwYTBtMmpxMzFsc25zcWI3In0.D0zCMdlhuyKVDAWG3zydKA', {
       attribution: '© Mapbox © OpenStreetMap',
       tileSize: 512,
@@ -487,115 +415,44 @@ async function startProcess() {
     /*submitBtn.style.bottom = '6220px'; //Random out of screen coordinates
     submitBtn.style.left = '1100px';*/
 
-});
+  });
 
     
     
-    map.on('click', function (e) {
-      if (!GameOver) {
-        latlng = e.latlng; 
-    
-        if (marker) {
-          map.removeLayer(marker);
-        }
-      
-        marker = L.marker(latlng).addTo(map);
-      
-        userLat = latlng.lat;
-        userLong = latlng.lng;
-        console.log(`Lat:${userLat}, Long:${userLong}`)
-        //console.log("Coordinates: " + latlng.lat + ", " + latlng.lng); // Log coordinates to console
-        //alert("Coordinates: " + latlng.lat + ", " + latlng.lng); // Show coordinates in an alert
+  map.on('click', function (e) {
+    if (!GameOver) {
+      latlng = e.latlng; 
+  
+      if (marker) {
+        map.removeLayer(marker);
       }
-    });
-  
-
-  
+    
+      marker = L.marker(latlng).addTo(map);
+    
+      userLat = latlng.lat;
+      userLong = latlng.lng;
+      console.log(`Lat:${userLat}, Long:${userLong}`)
+      //console.log("Coordinates: " + latlng.lat + ", " + latlng.lng); // Log coordinates to console
+      //alert("Coordinates: " + latlng.lat + ", " + latlng.lng); // Show coordinates in an alert
+    }
+  });
     //ClickAnswer
 
   
     submitBtn.addEventListener("click", () => {
-      //alert(`You Picked Lat: ${userLat}, Long: ${userLong}`);
-      GameOver = true;
-      let userdistance = haversineDistance(latitude,longitude,userLat,userLong);
-      document.getElementById('Distance').innerHTML = `You were ${userdistance} miles off!`;
-      document.getElementById('songName').innerHTML = `Song: '${songName}' by '${artistName}'`;
-      console.log(document.getElementById('songName').innerHTML);
-      //alert(`You were ${userdistance} miles off`);
-      
-      document.getElementById("pano").style.display = 'None';
-      submitBtn.style.display = "None";
-      gameOverScreen.style.display = "block";
-      document.getElementById('map').style.width = "60%";
-      document.getElementById('map').style.height = "45%";
-      document.getElementById('map').style.bottom = "50%";
-      document.getElementById('map').style.top = "30%";
-      document.getElementById('map').style.right = "20.5%";
-      
-      //document.getElementById('map').style.left = "50%";
-
-
-
-      ActualLatLong = {
-        lat: latitude,
-        lng: longitude
-      }
-      //Set up red point mark to mark the actual location
-      var redIcon = L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-        iconSize: [25, 41], 
-        iconAnchor: [12, 41], 
-        popupAnchor: [1, -34], 
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
-        shadowSize: [41, 41] 
-    });
-      ActualLocationMarker = L.marker(ActualLatLong,{icon: redIcon}).addTo(map);
-
-      map.removeLayer(marker);
-      console.log("SECOND LATLNG: ", latlng);
-      marker = L.marker(latlng).addTo(map);
-
-      
+      clearInterval(timer);
+      endGame();
     })
 
     //SpacebarAnswer
 
     document.addEventListener("keydown", (event) => {
       if (event.key===" "){
-      //alert(`You Picked Lat: ${userLat}, Long: ${userLong}`);
-      GameOver = true;
-      let userdistance = haversineDistance(latitude,longitude,userLat,userLong);
-      alert(`You were ${userdistance} miles off`);
-      
-      document.getElementById("pano").style.display = 'None';
-      submitBtn.style.display = "None";
-      gameOverScreen.style.display = "block";
-      document.getElementById('map').style.width = "60%";
-      document.getElementById('map').style.height = "45%";
-      document.getElementById('map').style.margin= "auto";
-
-      ActualLatLong = {
-        lat: latitude,
-        lng: longitude
+        clearInterval(timer);
+        endGame();
       }
-      //Set up red point mark to mark the actual location
-      var redIcon = L.icon({
-        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-        iconSize: [25, 41], 
-        iconAnchor: [12, 41], 
-        popupAnchor: [1, -34], 
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
-        shadowSize: [41, 41] 
-    });
-      ActualLocationMarker = L.marker(ActualLatLong,{icon: redIcon}).addTo(map);
-
-      map.removeLayer(marker);
-      marker = L.marker(latlng).addTo(map);
-
-      }
-
-
     })
+    
       //loading screen
   const checkStreetViewStatus = setInterval(() => {
     if (streetViewStatus) {
@@ -635,6 +492,80 @@ async function startProcess() {
     return distance;
 }
 
+
+
+function endGame() {
+  //alert(`You Picked Lat: ${userLat}, Long: ${userLong}`);
+  GameOver = true;
+  document.getElementById('timer').style.display = "none";
+  let userdistance = haversineDistance(latitude,longitude,userLat,userLong);
+  document.getElementById('Distance').innerHTML = `You were ${userdistance} miles off!`;
+  document.getElementById('songName').innerHTML = `Song: '${songName}' by '${artistName}'`;
+  console.log(document.getElementById('songName').innerHTML);
+  //alert(`You were ${userdistance} miles off`);
+  
+  document.getElementById("pano").style.display = 'None';
+  submitBtn.style.display = "None";
+  gameOverScreen.style.display = "block";
+  document.getElementById('map').style.width = "60%";
+  document.getElementById('map').style.height = "45%";
+  document.getElementById('map').style.bottom = "50%";
+  document.getElementById('map').style.top = "30%";
+  document.getElementById('map').style.right = "20.5%";
+  
+  //document.getElementById('map').style.left = "50%";
+
+
+
+  ActualLatLong = {
+    lat: latitude,
+    lng: longitude
+  }
+  //Set up red point mark to mark the actual location
+  var redIcon = L.icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    iconSize: [25, 41], 
+    iconAnchor: [12, 41], 
+    popupAnchor: [1, -34], 
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.4/images/marker-shadow.png',
+    shadowSize: [41, 41] 
+  });
+  ActualLocationMarker = L.marker(ActualLatLong,{icon: redIcon}).addTo(map);
+
+  map.removeLayer(marker);
+  console.log("SECOND LATLNG: ", latlng);
+  marker = L.marker(latlng).addTo(map);
+}
+
+document.getElementById('playAgain').addEventListener('click', async () => {
+  
+  // Clear map
+
+  map.removeLayer(ActualLocationMarker);
+  ActualLocationMarker = null; 
+  map.removeLayer(marker);
+  marker = null;
+  
+  // Hide the ending screen (gameOverScreen)
+  
+  document.getElementById('gameOverScreen').style.display = "none"
+  // show pano
+
+  document.getElementById("pano").style.display = 'block';
+  // set gameover to false
+
+  GameOver = false;
+
+  // move the minimap back into the correct position
+  
+  // Hide the pano (one-second image of previous location)
+  // move minimap back to bottom left corner
+
+ // document.getElementById("pano").innerHTML = "";
+  panorama = null;
+  startProcess()
+  //startProcess();
+})
 
 
 
